6348640b0e5b2b6dd0a6d209b76a18a12fe76b11
Kishan N <kishann380@gmail.com>
2018-02-06 14:44:23 +0530

    Removed Javascript elements from Form.cshtml and List.cshtml

---
+++ Moviedb/Scripts/Form.js
@@ -0,0 +1,41 @@
+﻿function SubmitProducer() {
+        $('#MyModalProducer').modal('toggle');
+
+    $.ajax({
+        type: "POST",
+            url: "/Producers/CreateProducer",
+            data: $("#ProducerForm").serialize(),
+            success: function (response) {
+                var x = document.getElementById("ProducersDropDown");
+
+                document.myResponse = response;
+
+                var option = document.createElement("option");
+                option.text = response.Name;
+                option.value = response.Id;
+                x.add(option);
+            }
+        });
+    }
+function SubmitActor() {
+        $('#MyModalActor').modal('toggle');
+
+    $.ajax({
+        type: "POST",
+            url: "/Actors/CreateActor",
+            data: $("#ActorForm").serialize(),
+            success: function (response) {
+        console.log("Success submit actor");
+    document.response = response;
+                var x = document.getElementsByClassName("actor_dropdown")[0];
+                var option = document.createElement("option");
+                option.text = response.Name;
+                option.value = response.Id;
+                x.add(option);
+
+                $('.actor_dropdown').trigger("chosen:updated");
+            }
+
+        });
+
+    }
---
+++ Moviedb/Scripts/Index.js
@@ -0,0 +1,18 @@
+﻿function deleteMovie(id) {
+
+    var delMovie = confirm("Do you want to delete the movie");
+
+    if (delMovie === true) {
+        $.ajax({
+            type: "POST",
+            url: "/Movies/DeleteAjax",
+            data: { id },
+            success: function (response) {
+                document.MyResponseDelete = response;
+                console.log("Success");
+                var row = document.getElementById(id);
+                row.parentNode.removeChild(row);
+            }
+        });
+    }
+}
